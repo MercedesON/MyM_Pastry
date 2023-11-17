c85a93f208e97408756c649a753915fe36aedbef
MercedesON <mercedes.o.n.08@gmail.com>
2023-11-17 15:32:23 -0500

    carousel-snacks-swiper

--- client/src/app/components/snacks/snacks.component.ts
+++ client/src/app/components/snacks/snacks.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import SwiperCore, {Navigation, Pagination, Scrollbar, A11y, SwiperOptions, Swiper} from 'swiper';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 @Component({
   selector: 'app-snacks',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./snacks.component.scss']
 })
 export class SnacksComponent {
-
+  config: SwiperOptions = {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 50,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+ };
+slides = [
+  {
+       link: 'https://www.google.com',
+       image: 'assets/images/empanadas.jpg',
+  },
+  {
+       link: 'https://www.youtube.com',
+       image: 'assets/images/minipizza.jpg',
+  },
+  {
+       link: 'https://www.facebook.com',
+       image: 'assets/images/pitipanes.jpg',
+  },
+  {
+    link: 'https://www.facebook.com',
+    image: 'assets/images/triple.jpg',
+}
+  ];
+title = 'Swiper';
 }
